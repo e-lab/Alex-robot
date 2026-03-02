554423ff748333b64e0c64825956e1071aae6f7b
Eugenio Culurciello <culurciello@gmail.com>
2026-03-02 16:18:07 -0500

    added right camera

--- scenes/alex-scenes/scene_alex_v1_full_body_mjx_room1.py
+++ scenes/alex-scenes/scene_alex_v1_full_body_mjx_room1.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 import time
-
 import mujoco
 import mujoco.viewer
-
 
 def main() -> None:
     xml_path = (Path(__file__).resolve().parent / "scene_alex_v1_full_body_mjx_room1.xml").as_posix()
@@ -15,6 +13,14 @@ def main() -> None:
     mujoco.mj_forward(model, data)  # recompute kinematics after qpos change
 
     with mujoco.viewer.launch_passive(model, data) as viewer:
+
+        # use camera main view:
+        viewer.cam.type = mujoco.mjtCamera.mjCAMERA_FIXED
+        with viewer.lock():
+            viewer.cam.fixedcamid = mujoco.mj_name2id(
+                model, mujoco.mjtObj.mjOBJ_CAMERA, "main"
+            )
+
         while viewer.is_running():
             step_start = time.time()
             mujoco.mj_step(model, data)
@@ -22,7 +28,6 @@ def main() -> None:
             remaining = model.opt.timestep - (time.time() - step_start)
             if remaining > 0:
                 time.sleep(remaining)
-
 
 if __name__ == "__main__":
     main()
